ce03fbd5309fa4931764cca2e9264ef8b86e0161
Andriy Ivashchenko <andriy.ivashchenko@thomsonreuters.com>
2018-10-18 15:17:54 +0300

    add route splitting

--- src/js/components/MainMenu/MainMenu.js
+++ src/js/components/MainMenu/MainMenu.js
@@ -1,48 +1,51 @@
 // @flow
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import styles from './MainMenu.scss';
 
-const MainMenu = () => (
-  <nav className={styles.topmenu}>
-    <NavLink
-      exact
-      activeClassName={styles.active}
-      className={styles.link}
-      to='/'
-      >
-      Home
-    </NavLink>
-    <NavLink
-      activeClassName={styles.active}
-      className={styles.link}
-      to='/about'
-      >
-      About
-    </NavLink>
-    <NavLink
-      activeClassName={styles.active}
-      className={styles.link}
-      to='/gallery'
-      >
-      Gallery
-    </NavLink>
-    <NavLink
-      activeClassName={styles.active}
-      className={styles.link}
-      to='/works'
-      >
-      Works
-    </NavLink>
-    <NavLink
-      activeClassName={styles.active}
-      className={styles.link}
-      to='/contacts'
-      >
-      Contacts
-    </NavLink>
-  </nav>
-);
+const MainMenu = (props) => {
+  console.log(props);
+  return (
+    <nav className={styles.topmenu}>
+      <NavLink
+        exact
+        activeClassName={styles.active}
+        className={styles.link}
+        to='/'
+        >
+        Home
+      </NavLink>
+      <NavLink
+        activeClassName={styles.active}
+        className={styles.link}
+        to='/about'
+        >
+        About
+      </NavLink>
+      <NavLink
+        activeClassName={styles.active}
+        className={styles.link}
+        to='/gallery'
+        >
+        Gallery
+      </NavLink>
+      <NavLink
+        activeClassName={styles.active}
+        className={styles.link}
+        to='/works'
+        >
+        Works
+      </NavLink>
+      <NavLink
+        activeClassName={styles.active}
+        className={styles.link}
+        to='/contacts'
+        >
+        Contacts
+      </NavLink>
+    </nav>
+  );
+};
 
-export default MainMenu;
+export default withRouter(MainMenu);
